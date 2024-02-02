bafb31d6662cc384b48e3491db751816f7ddacd1
Leonardobern <leonardo.bernardo2658@gmail.com>
2024-02-02 15:21:31 -0300

    new file:   assets/scripts/menu.js modified:   assets/styles/header.css modified:   index.html

---
+++ assets/scripts/menu.js
@@ -0,0 +1,19 @@
+const buttonMenu = document.getElementsByClassName("menu_head")[0]
+
+buttonMenu.addEventListener("click", function() {
+    console.log("CLicou")
+    const existingMenu = buttonMenu.querySelector(".content_menu");
+
+    if(!existingMenu){
+        const menu = document.createElement("div")
+        menu.setAttribute("class", "content_menu")
+        buttonMenu.appendChild(menu)
+        console.log(menu)
+    }
+    else {
+        console.log(buttonMenu)
+        buttonMenu.removeChild(buttonMenu.firstChild)
+    }
+});
+
+
