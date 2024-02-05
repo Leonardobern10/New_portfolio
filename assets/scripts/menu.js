883f2da467f5694364620e3b91c6badbbc8156f0
Leonardobern <leonardo.bernardo2658@gmail.com>
2024-02-05 12:01:02 -0300

    Atualização do Site Pessoal.

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,19 +1,16 @@
-const buttonMenu = document.getElementsByClassName("menu_head")[0]
+const buttonMenu = document.getElementsByClassName("menu_head")[0];
 
-buttonMenu.addEventListener("click", function() {
-    console.log("CLicou")
-    const existingMenu = buttonMenu.querySelector(".content_menu");
+buttonMenu.addEventListener("click", function () {
+  console.log("CLicou");
+  const existingMenu = buttonMenu.querySelector(".content_menu");
 
-    if(!existingMenu){
-        const menu = document.createElement("div")
-        menu.setAttribute("class", "content_menu")
-        buttonMenu.appendChild(menu)
-        console.log(menu)
-    }
-    else {
-        console.log(buttonMenu)
-        buttonMenu.removeChild(buttonMenu.firstChild)
-    }
+  if (!existingMenu) {
+    const menu = document.createElement("div");
+    menu.setAttribute("class", "content_menu");
+    buttonMenu.appendChild(menu);
+    console.log(menu);
+  } else {
+    console.log(buttonMenu);
+    buttonMenu.removeChild(buttonMenu.firstChild);
+  }
 });
-
-
